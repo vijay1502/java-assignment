76eb67f61903d1f64121c11be4fd270003ac3c4e
vijay <vijaya.kanugovi@cgi.com>
2019-07-20 15:53:54 +0530

    Class Condition based output changed

--- src/main/java/com/stackroute/pe1/ConditionBasedOutput.java
+++ src/main/java/com/stackroute/pe1/ConditionBasedOutput.java
@@ -2,16 +2,16 @@ package com.stackroute.pe1;
 
 import java.util.*;
 public class ConditionBasedOutput {
-    public static void main(String[] args){
+   /* public static void main(String[] args){
         System.out.println("Enter an Integer :");
         Scanner scanner= new Scanner(System.in);
         int number =scanner.nextInt();
         ConditionBasedOutput t=new ConditionBasedOutput();
         t.compareCondition(number);
-    }
+    }*/
 
         public String compareCondition(int formalNumber){
-        if(formalNumber>20&&formalNumber<30) {
+        if(formalNumber>=20&&formalNumber<=30) {
             if (formalNumber % 2 == 0)
                 return ("jerry");
 
